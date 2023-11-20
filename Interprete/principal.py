f3c4344b85ad7c42675947c8cbd1ab61470c5574
luisk0o0 <luiscastroj2000@gmail.com>
2023-11-19 20:36:35 -0500

    update

--- Interprete/principal.py
+++ Interprete/principal.py
@@ -3,91 +3,175 @@ import ts as TS
 from expresiones import *
 from instrucciones import *
 
-def procesar_imprimir(instr, ts) :
-    print('> ', resolver_cadena(instr.cad, ts))
+class ProcesadorCodigo:
 
-def procesar_definicion(instr, ts) :
-    simbolo = TS.Simbolo(instr.id, TS.TIPO_DATO.NUMERO, 0)      # inicializamos con 0 como valor por defecto
-    ts.agregar(simbolo)
+    def __init__(self):
+        self.ts_global = TS.TablaDeSimbolos()
 
-def procesar_asignacion(instr, ts) :
-    val = resolver_expresion_aritmetica(instr.expNumerica, ts)
-    simbolo = TS.Simbolo(instr.id, TS.TIPO_DATO.NUMERO, val)
-    ts.actualizar(simbolo)
+    def procesar_imprimir(self, instr):
+        print('> ', self.resolver_cadena(instr.cad))
 
-def procesar_mientras(instr, ts) :
-    while resolver_expreision_logica(instr.expLogica, ts) :
-        ts_local = TS.TablaDeSimbolos(ts.simbolos)
-        procesar_instrucciones(instr.instrucciones, ts_local)
+    def procesar_definicion(self, instr):
+        simbolo = TS.Simbolo(instr.id, TS.TIPO_DATO.NUMERO, 0)  # inicializamos con 0 como valor por defecto
+        self.ts_global.agregar(simbolo)
 
-def procesar_SI(instr, ts) :
-    val = resolver_expreision_logica(instr.expLogica, ts)
-    if val:
-        ts_local = TS.TablaDeSimbolos(ts.simbolos)
-        procesar_instrucciones(instr.instrucciones, ts_local)
+    def procesar_asignacion(self, instr):
+        val = self.resolver_expresion_aritmetica(instr.expNumerica)
+        simbolo = TS.Simbolo(instr.id, TS.TIPO_DATO.NUMERO, val)
+        self.ts_global.actualizar(simbolo)
 
-def procesar_SINO(instr, ts) :
-    val = resolver_expreision_logica(instr.expLogica, ts)
-    if val :
-        ts_local = TS.TablaDeSimbolos(ts.simbolos)
-        procesar_instrucciones(instr.instrIfVerdadero, ts_local)
-    else :
-        ts_local = TS.TablaDeSimbolos(ts.simbolos)
-        procesar_instrucciones(instr.instrIfFalso, ts_local)
+    def procesar_mientras(self, instr):
+        while self.resolver_expreision_logica(instr.expLogica):
+            ts_local = TS.TablaDeSimbolos(self.ts_global.simbolos)
+            self.procesar_instrucciones(instr.instrucciones, ts_local)
 
-def resolver_cadena(expCad, ts) :
-    if isinstance(expCad, ExpresionConcatenar) :
-        exp1 = resolver_cadena(expCad.exp1, ts)
-        exp2 = resolver_cadena(expCad.exp2, ts)
-        return exp1 + exp2
-    elif isinstance(expCad, ExpresionDobleComilla) :
-        return expCad.val
-    elif isinstance(expCad, ExpresionCadenaNumerico) :
-        return str(resolver_expresion_aritmetica(expCad.exp, ts))
-    else :
-        print('Error: Expresión cadena no válida')
+    def procesar_SI(self, instr):
+        val = self.resolver_expreision_logica(instr.expLogica)
+        if val:
+            ts_local = TS.TablaDeSimbolos(self.ts_global.simbolos)
+            self.procesar_instrucciones(instr.instrucciones, ts_local)
 
+    def procesar_SINO(self, instr):
+        val = self.resolver_expreision_logica(instr.expLogica)
+        if val:
+            ts_local = TS.TablaDeSimbolos(self.ts_global.simbolos)
+            self.procesar_instrucciones(instr.instrIfVerdadero, ts_local)
+        else:
+            ts_local = TS.TablaDeSimbolos(self.ts_global.simbolos)
+            self.procesar_instrucciones(instr.instrIfFalso, ts_local)
 
-def resolver_expreision_logica(expLog, ts) :
-    exp1 = resolver_expresion_aritmetica(expLog.exp1, ts)
-    exp2 = resolver_expresion_aritmetica(expLog.exp2, ts)
-    if expLog.operador == OPERACION_LOGICA.MAYOR_QUE : return exp1 > exp2
-    if expLog.operador == OPERACION_LOGICA.MENOR_QUE : return exp1 < exp2
-    if expLog.operador == OPERACION_LOGICA.IGUAL : return exp1 == exp2
-    if expLog.operador == OPERACION_LOGICA.DIFERENTE : return exp1 != exp2
+    def resolver_cadena(self, expCad):
+        if isinstance(expCad, ExpresionConcatenar):
+            exp1 = self.resolver_cadena(expCad.exp1)
+            exp2 = self.resolver_cadena(expCad.exp2)
+            return exp1 + exp2
+        elif isinstance(expCad, ExpresionDobleComilla):
+            return expCad.val
+        elif isinstance(expCad, ExpresionCadenaNumerico):
+            return str(self.resolver_expresion_aritmetica(expCad.exp))
+        else:
+            print('Error: Expresión cadena no válida')
 
-def resolver_expresion_aritmetica(expNum, ts) :
-    if isinstance(expNum, ExpresionBinaria) :
-        exp1 = resolver_expresion_aritmetica(expNum.exp1, ts)
-        exp2 = resolver_expresion_aritmetica(expNum.exp2, ts)
-        if expNum.operador == OPERACION_ARITMETICA.MAS : return exp1 + exp2
-        if expNum.operador == OPERACION_ARITMETICA.MENOS : return exp1 - exp2
-        if expNum.operador == OPERACION_ARITMETICA.POR : return exp1 * exp2
-        if expNum.operador == OPERACION_ARITMETICA.DIVIDIDO : return exp1 / exp2
-    elif isinstance(expNum, ExpresionNegativo) :
-        exp = resolver_expresion_aritmetica(expNum.exp, ts)
-        return exp * -1
-    elif isinstance(expNum, ExpresionNumero) :
-        return expNum.val
-    elif isinstance(expNum, ExpresionIdentificador) :
-        return ts.obtener(expNum.id).valor
+    def resolver_expreision_logica(self, expLog):
+        exp1 = self.resolver_expresion_aritmetica(expLog.exp1)
+        exp2 = self.resolver_expresion_aritmetica(expLog.exp2)
+        if expLog.operador == OPERACION_LOGICA.MAYOR_QUE:
+            return exp1 > exp2
+        if expLog.operador == OPERACION_LOGICA.MENOR_QUE:
+            return exp1 < exp2
+        if expLog.operador == OPERACION_LOGICA.IGUAL:
+            return exp1 == exp2
+        if expLog.operador == OPERACION_LOGICA.DIFERENTE:
+            return exp1 != exp2
 
+    def resolver_expresion_aritmetica(self, expNum):
+        if isinstance(expNum, ExpresionBinaria):
+            exp1 = self.resolver_expresion_aritmetica(expNum.exp1)
+            exp2 = self.resolver_expresion_aritmetica(expNum.exp2)
+            if expNum.operador == OPERACION_ARITMETICA.MAS:
+                return exp1 + exp2
+            if expNum.operador == OPERACION_ARITMETICA.MENOS:
+                return exp1 - exp2
+            if expNum.operador == OPERACION_ARITMETICA.POR:
+                return exp1 * exp2
+            if expNum.operador == OPERACION_ARITMETICA.DIVIDIDO:
+                return exp1 / exp2
+        elif isinstance(expNum, ExpresionNegativo):
+            exp = self.resolver_expresion_aritmetica(expNum.exp)
+            return exp * -1
+        elif isinstance(expNum, ExpresionNumero):
+            return expNum.val
+        elif isinstance(expNum, ExpresionIdentificador):
+            return self.ts_global.obtener(expNum.id).valor
 
-def procesar_instrucciones(instrucciones, ts) :
-    ## lista de instrucciones recolectadas
-    for instr in instrucciones :
-        if isinstance(instr, Imprimir) : procesar_imprimir(instr, ts)
-        elif isinstance(instr, Definicion) : procesar_definicion(instr, ts)
-        elif isinstance(instr, Asignacion) : procesar_asignacion(instr, ts)
-        elif isinstance(instr, Mientras) : procesar_mientras(instr, ts)
-        elif isinstance(instr, SI) : procesar_SI(instr, ts)
-        elif isinstance(instr, SINO) : procesar_SINO(instr, ts)
-        else : print('Error: instrucción no válida')
+    def procesar_instrucciones(self, instrucciones, ts):
+        for instr in instrucciones:
+            if isinstance(instr, Imprimir):
+                self.procesar_imprimir(instr)
+            elif isinstance(instr, Definicion):
+                self.procesar_definicion(instr)
+            elif isinstance(instr, Asignacion):
+                self.procesar_asignacion(instr)
+            elif isinstance(instr, Mientras):
+                self.procesar_mientras(instr)
+            elif isinstance(instr, SI):
+                self.procesar_SI(instr)
+            elif isinstance(instr, SINO):
+                self.procesar_SINO(instr)
+            else:
+                print('Error: instrucción no válida')
 
-f = open("./entrada.txt", "r")
-input = f.read()
+#f = open("./entrada.txt", "r")
+#input = f.read()
 
-instrucciones = g.parse(input)
-ts_global = TS.TablaDeSimbolos()
+#instrucciones = g.parse(input)
+#ts_global = TS.TablaDeSimbolos()
 
-procesar_instrucciones(instrucciones, ts_global)
+#procesar_instrucciones(instrucciones, ts_global)
+
+import tkinter as tk
+from tkinter import scrolledtext
+
+class Interfaz:
+
+    def __init__(self, root, procesador):
+        self.root = root
+        self.procesador = procesador
+        root.title("Interfaz de Ejecución")
+
+        # Texto del código
+        self.codigo_label = tk.Label(root, text="Código:")
+        self.codigo_label.pack()
+
+        self.codigo_text = scrolledtext.ScrolledText(root, width=60, height=10)
+        self.codigo_text.pack()
+
+        # Botón de ejecución
+        self.ejecutar_boton = tk.Button(root, text="Ejecutar Código", command=self.ejecutar_codigo)
+        self.ejecutar_boton.pack()
+
+        # Resultado en la consola
+        self.consola_label = tk.Label(root, text="Resultado:")
+        self.consola_label.pack()
+
+        self.consola_text = scrolledtext.ScrolledText(root, width=60, height=10)
+        self.consola_text.pack()
+
+        # Ejemplo de código desde el archivo entrada.txt
+        self.ejemplo_label = tk.Label(root, text="Ejemplo:")
+        self.ejemplo_label.pack()
+
+        with open("entrada.txt", "r") as archivo:
+            ejemplo_codigo = archivo.read()
+
+        self.ejemplo_text = scrolledtext.ScrolledText(root, width=60, height=20)
+        self.ejemplo_text.insert(tk.END, ejemplo_codigo)
+        self.ejemplo_text.pack()
+
+    def ejecutar_codigo(self):
+        codigo = self.codigo_text.get("1.0", tk.END)
+        instrucciones = g.parse(codigo)
+        
+        # Limpiar la consola antes de ejecutar nuevo código
+        self.consola_text.delete("1.0", tk.END)
+
+        # Redirigir la salida estándar a un buffer para capturarla
+        import sys
+        original_stdout = sys.stdout
+        sys.stdout = self
+
+        self.procesador.procesar_instrucciones(instrucciones, self.procesador.ts_global)
+
+        # Restaurar la salida estándar
+        sys.stdout = original_stdout
+
+    def write(self, text):
+        # Método necesario para redirigir la salida estándar
+        self.consola_text.insert(tk.END, text)
+
+# Crear una instancia de ProcesadorCodigo y de la Interfaz
+if __name__ == "__main__":
+    procesador = ProcesadorCodigo()
+    root = tk.Tk()
+    interfaz = Interfaz(root, procesador)
+    root.mainloop()
